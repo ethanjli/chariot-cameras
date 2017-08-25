996601641e83a345fe20102b1248971e43db45b0
Ethan Li <lietk12@gmail.com>
2017-08-25 08:18:08 +0000

    Added command-line arg to quit rather than halt.

--- client/raspicam.py
+++ client/raspicam.py
@@ -12,18 +12,21 @@ import sockets
 CAMERA_POSITIONS = ['front', 'rear', 'left', 'right']
 
 class Handlers(sockets.StandardHandlers):
-    def __init__(self, socket, position):
+    def __init__(self, socket, position, quit):
         super(Handlers, self).__init__(socket, 'camera', 'raspicam-' + position)
         self.camera = picamera.PiCamera()
         self.camera.vflip = True
         self.camera.hflip = True
-	self.camera.resolution = (1280, 960)
+        self.camera.resolution = (1280, 960)
+        self.quit = quit
 
     def shutdown(self):
         print('[Recording] Shutting down...')
         self.camera.close()
-        sys.exit()
-        #subprocess.call('sudo shutdown -h now', shell=True)
+        if self.quit:
+            sys.exit()
+        else:
+            subprocess.call('sudo shutdown -h now', shell=True)
 
     def start(self):
         print('[Recording] Starting recording...')
@@ -34,13 +37,16 @@ class Handlers(sockets.StandardHandlers):
         self.camera.stop_recording()
 
 def main(args):
-    sockets.listen_event_handlers(args.server, Handlers, args.position)
+    sockets.listen_event_handlers(args.server, Handlers,
+                                  args.position, args.quit)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('position', type=str, choices=CAMERA_POSITIONS,
                         help='Position of camera.')
+    parser.add_argument('--quit', action="store_true", default=True,
+                        help='Quit instead of shutting down the system.')
     sockets.add_server_arg(parser, default_server='tessel')
     main(parser.parse_args())
 
